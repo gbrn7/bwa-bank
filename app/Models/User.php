8b98a5b0308c470c5839bbc84e15296a2e9cbe47
gbrn7 <mraygbrn@gmail.com>
2023-12-25 17:45:59 +0700

    feat(model): created bwa-bank models

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
+        'profile_picture',
+        'ktp',
+        'verified',
     ];
 
     /**
