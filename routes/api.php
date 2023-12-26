9fd5900366a954efb7dcd689a72df38bc6f1c189
gbrn7 <mraygbrn@gmail.com>
2023-12-26 16:51:09 +0700

    feat(api): created Dataplan store api

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TopUpController;
 use App\Http\Controllers\Api\WebhookController;
 use App\Http\Controllers\Api\TransferController;
+use App\Http\Controllers\Api\DataPlanController;
 
 
 /*
@@ -27,4 +28,5 @@ Route::post('webhooks', [WebhookController::class, 'update']);
 Route::middleware(['middleware' => 'jwt.verify'])->group(function ($router) {
     Route::post('top_ups', [TopUpController::class, 'store']);
     Route::post('transfers', [TransferController::class, 'store']);
+    Route::post('data_plans', [DataPlanController::class, 'store']);
 });
